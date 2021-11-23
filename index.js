792c4381beb0e2868ec1610506d510a7975d04cb
Defne Celikoyar <defne.celikoyar@gmail.com>
2021-11-22 22:38:19 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1=31;
+let num2=2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random());
+
+let num3=69;
+let num4=13;
+let mod = num3 % num4;
+
+let max = Math.max(13, 14, 15, 16, 17, 18, 19, 20);
+
+// Create a variable called max that finds the highest number in a set; the value returned should be 20.
